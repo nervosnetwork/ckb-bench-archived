b51e1bc8ca76468bda7e1d52711fb42b5f4e08e5
quake wang <quake.wang@gmail.com>
2019-06-20 13:13:32 +0800

    chore: add bastion and bootnode

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -26,18 +26,13 @@ resource "aws_route" "internet_access" {
     gateway_id             = "${aws_internet_gateway.ig.id}"
 }
 
-resource "aws_subnet" "subnet" {
-    vpc_id                  = "${aws_vpc.vpc.id}"
-    cidr_block              = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "${var.prefix}-subnet"
-    }
+resource "aws_key_pair" "ssh" {
+    key_name = "ssh_key"
+    public_key = "${file(var.public_key_path)}"
 }
 
-resource "aws_security_group" "sg" {
-    name        = "${var.prefix}-default-sg"
-    description = "Allow inbound ssh access, inbound all vpc access and outbound all access"
+resource "aws_security_group" "bastion-sg" {
+    name        = "${var.prefix}-bastion-sg"
     vpc_id      = "${aws_vpc.vpc.id}"
 
     ingress {
@@ -47,15 +42,6 @@ resource "aws_security_group" "sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    # inbound access from the VPC
-    ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["10.0.0.0/16"]
-    }
-
-    # outbound access to all
     egress {
         from_port   = 0
         to_port     = 0
@@ -63,11 +49,100 @@ resource "aws_security_group" "sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
-resource "aws_key_pair" "ssh" {
-    key_name = "ssh_key"
-    public_key = "${file(var.public_key_path)}"
+
+# Create a bastion instance, this instance will be used as workload generator also.
+resource "aws_instance" "bastion" {
+    key_name                    = "${aws_key_pair.ssh.id}"
+    instance_type               = "${var.instance_type}"
+    ami                         = "${data.aws_ami.ubuntu.id}"
+    vpc_security_group_ids      = ["${aws_security_group.bastion-sg.id}"]
+    private_ip                  = "10.0.1.100"
+    subnet_id                   = "${aws_subnet.subnet.id}"
+    associate_public_ip_address = true
+
+    root_block_device {
+        volume_size = "60"
+    }
+
+    connection {
+        host        = "${aws_instance.bastion.public_ip}"
+        user        = "ubuntu"
+        private_key = "${file(var.private_key_path)}"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+        "sudo apt -y update",
+        "sudo apt -y install python-minimal",
+        ]
+    }
+
+    tags = {
+        Name = "${var.prefix}-bastion"
+    }
 }
 
+resource "aws_subnet" "subnet" {
+    vpc_id                  = "${aws_vpc.vpc.id}"
+    cidr_block              = "10.0.1.0/24"
+    tags = {
+        Name = "${var.prefix}-subnet"
+    }
+}
+
+resource "aws_security_group" "sg" {
+    name        = "${var.prefix}-default-sg"
+    description = "Allow inbound access from vpc and outbound access to all"
+    vpc_id      = "${aws_vpc.vpc.id}"
+
+    ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["${aws_vpc.vpc.cidr_block}"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Create a bootnode instance with fix private ip
+resource "aws_instance" "bootnode" {
+    key_name               = "${aws_key_pair.ssh.id}"
+    instance_type          = "${var.instance_type}"
+    ami                    = "${data.aws_ami.ubuntu.id}"
+    vpc_security_group_ids = ["${aws_security_group.sg.id}"]
+    private_ip             = "10.0.1.101"
+    subnet_id              = "${aws_subnet.subnet.id}"
+
+    root_block_device {
+        volume_size = "60"
+    }
+
+    connection {
+        bastion_host = "${aws_instance.bastion.public_ip}"
+        host         = "${aws_instance.bootnode.private_ip}"
+        user         = "ubuntu"
+        private_key  = "${file(var.private_key_path)}"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+        "sudo apt -y update",
+        "sudo apt -y install python-minimal",
+        ]
+    }
+
+    tags = {
+        Name = "${var.prefix}-bootnode"
+    }
+}
+
+# Create other normal instances
 resource "aws_instance" "instance" {
     count                  = "${var.instance_count}"
     key_name               = "${aws_key_pair.ssh.id}"
@@ -93,10 +168,10 @@ resource "null_resource" "instance_provisioners" {
     }
 
     connection {
-        host        = "${element(aws_instance.instance.*.public_ip, count.index)}"
-        # The default username for our AMI
-        user        = "ubuntu"
-        private_key = "${file(var.private_key_path)}"
+        bastion_host = "${aws_instance.bastion.public_ip}"
+        host         = "${element(aws_instance.instance.*.private_ip, count.index)}"
+        user         = "ubuntu"
+        private_key  = "${file(var.private_key_path)}"
     }
 
     # Install python for ansible
@@ -108,6 +183,10 @@ resource "null_resource" "instance_provisioners" {
     }
 }
 
-output "public_ips" {
-    value = "${join("\n", aws_instance.instance.*.public_ip)}"
+output "bastion_public_ip" {
+    value = "${aws_instance.bastion.public_ip}"
+}
+
+output "instance_private_ips" {
+    value = "${join("\n", aws_instance.instance.*.private_ip)}"
 }
